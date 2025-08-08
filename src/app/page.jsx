f60fbdfa1422baa959a945da29c6b20cb70d39c4
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2025-08-07 22:44:31 -0300

    styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,16 @@
+import styles from './page.module.css';
+
 export default function Home() {
   return(
-    <div>
-      <h1>Hello Word!!</h1>
+    <div className="container">
+      <h1 className={styles.tituloHome}>Titulo 1</h1>
+      <h2>Titulo 2</h2>
+      <h3>Titulo 3</h3>
+      <p className="txtDestaque">Primeiro exemplo no Next</p>
       <p>Primeiro exemplo no Next</p>
+      <small>Pequeno texto</small>
+      <br />
+      <div className="quadrado"></div>
     </div>
     
   )
